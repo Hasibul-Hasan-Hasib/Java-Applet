83cb5851f793f96a7bf0b791820025390823567c
Hasibul Hasan Hasib <mdhasibulctg100@gmail.com>
2021-09-28 14:02:12 +0600

    java applet

---
+++ Applete/src/NewApplet2A.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+
+import java.awt.Graphics;
+
+public class NewApplet2A extends Applet
+{
+    public void paint(Graphics g)
+    {
+        g.drawString("Hasibul Hasan Hasib",150,100);
+    }
+}
